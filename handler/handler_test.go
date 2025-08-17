3fc2c885f36f3ef47353b479252c2c1cd1b675e9
Fisherman August <fshrmn.august@gmail.com>
2025-08-17 11:31:11 +0300

    http standart lib test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,7 +1,14 @@
 package handler
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStatusHandler(t *testing.T) {
@@ -26,7 +33,17 @@ func TestStatusHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			StatusHandler(tt.args.w, tt.args.r)
+			request := httptest.NewRequest(http.MethodGet, "/status", nil)
+			w := httptest.NewRecorder()
+			StatusHandler(w, request)
+			res := w.Result()
+			assert.Equal(t, tt.want.code, res.StatusCode)
+			defer res.Body.Close()
+			//Выше рассмотрели
+			resBody, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, tt.want.response, string(resBody))
+			assert.Equal(t, tt.want.contentType, res.Header.Get("Content-Type"))
 		})
 	}
 }
