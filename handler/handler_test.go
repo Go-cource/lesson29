0785071ce94ec56744c87ceee4472e73ede228a4
Fisherman August <fshrmn.august@gmail.com>
2025-08-17 12:16:49 +0300

    allure test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,49 +1,26 @@
 package handler
 
 import (
-	"io"
+	"context"
 	"net/http"
-	"net/http/httptest"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/ozontech/cute"
+	"github.com/ozontech/cute/asserts/json"
 )
 
 func TestStatusHandler(t *testing.T) {
-	type want struct {
-		code        int
-		response    string
-		contentType string
-	}
-	tests := []struct {
-		name string
-		want want
-	}{
-		// TODO: Add test cases.
-		{
-			name: "Simple test #1",
-			want: want{
-				code:        200,
-				response:    `{"status": "ok"}`,
-				contentType: "application/json",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodGet, "/status", nil)
-			w := httptest.NewRecorder()
-			StatusHandler(w, request)
-			res := w.Result()
-			assert.Equal(t, tt.want.code, res.StatusCode)
-			defer res.Body.Close()
-			//Выше рассмотрели
-			resBody, err := io.ReadAll(res.Body)
-			require.NoError(t, err)
-			assert.JSONEq(t, tt.want.response, string(resBody))
-			assert.Equal(t, tt.want.contentType, res.Header.Get("Content-Type"))
-		})
-	}
+	cute.NewTestBuilder().
+		Title("Simple test #1").
+		Description("Simple get request to '/status'").
+		Create().
+		RequestBuilder(
+			cute.WithURI("http://127.0.0.1:8080/status"),
+			cute.WithMethod(http.MethodGet),
+		).ExpectExecuteTimeout(10*time.Second).
+		ExpectStatus(http.StatusOK).
+		AssertBody(
+			json.Equal("$.status", "ok"),
+		).ExecuteTest(context.Background(), t)
 }
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/status", hd.StatusHandler)
+
 	fmt.Println("Server starting...")
 	http.ListenAndServe(":8080", nil)
 }
