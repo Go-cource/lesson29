85f59678a87398ca65ce2287bb2e88ce768eb253
Fisherman August <fshrmn.august@gmail.com>
2025-08-17 10:44:19 +0300

    auto test generating

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	hd "httptesting/handler"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/status", hd.StatusHandler)
+	fmt.Println("Server starting...")
 	http.ListenAndServe(":8080", nil)
 }
