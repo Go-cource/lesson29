2bee6723ae6fe4fed1d6330917a432a6b9c9f5ac
August Fisherman <fshrmn.august@gmail.com>
2026-01-22 19:35:40 +0300

    pass tests

--- handler/handler.go
+++ handler/handler.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"status":"ok}`))
+	w.Write([]byte(`{"status":"ok"}`))
 }
