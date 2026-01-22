7fccee8d17ddc5d26f6d729b36849d838c141b90
August Fisherman <fshrmn.august@gmail.com>
2026-01-22 19:00:31 +0300

    simple server

--- handler/handler.go
+++ handler/handler.go
@@ -3,5 +3,6 @@ package handler
 import "net/http"
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"status":"ok"}`))
 }
